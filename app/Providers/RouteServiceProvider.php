eaac24e311d7f3a78c7d33d6b3f5e82876807dc4
Hasib Islam <hasib.pust.cse@gmail.com>
2025-03-10 00:26:12 +0600

    update route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,6 +38,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+            Route::middleware(['api']) // User API Routes (v1)
+                ->prefix('api/v1')
+                ->group(base_path('routes/api/v1/auth.php'));
+            Route::middleware(['api']) // User API Routes (v1)
+                ->prefix('api/v1')
+                ->group(base_path('routes/api/v1/api.php'));
         });
     }
 
